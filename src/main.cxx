9928a1864218373e9b1b24f2d58a1a41df21901a
Thiago Nunes <gorobaum@gmail.com>
2014-03-12 11:13:56 -0300

    I has image

--- src/main.cxx
+++ src/main.cxx
@@ -2,7 +2,16 @@
 #include <cstdlib>
 #include <cstdio>
 
+#include "CImg.h"
+using namespace cimg_library;
+
 int main(int argc, char** argv) {
-	printf("DAE EU AINDA LEMBRO COMO FAS\n");
+	if (argc != 2) {
+		printf("Precisa passar o nome da imagem né coração?\n");
+		return -1;
+	} 
+	CImg<unsigned char> image("teste.jpg");
+	CImgDisplay main_disp(image,"Click a point");
+	while (!main_disp.is_closed()) {}
 	return 0;
 }
