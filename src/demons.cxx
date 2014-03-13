8ce64949996d879b8e634c2810fb6ad840e3dbbd
Thiago Nunes <gorobaum@gmail.com>
2014-03-13 15:36:51 -0300

    Trying to create the gradient

--- src/demons.cxx
+++ src/demons.cxx
@@ -1,5 +1,6 @@
 
 #include <cstdio>
+#include <cstdlib>
 
 #include "demons.h"
 
@@ -15,6 +16,15 @@ Demons::Vector* Demons::findGrad() {
 	const CImg<float> maskX(3,3,1,1, 1,0,-1, 2,0,-2,1,0,-1), maskY = maskX.get_transpose();
   	const CImg<float> imgX = staticImage_.get_convolve(maskX), imgY = staticImage_.get_convolve(maskY);
   	(imgX,imgY).display("ImgX, imgY");
-	Demons::Vector* macaco;
-	return macaco;
+  	int width = staticImage_.width(), height = staticImage_.height();
+	Vector* grad = (Vector*)malloc(width*height*sizeof(Vector));
+	for (int y = 0; y < staticImage_.height(); y++) {
+		for (int x = 0; x < staticImage_.width(); x++) {
+			std::vector<float> newVector(imgX.atXY(x, y, 0, 0, 0.0), imgY.atXY(x, y, 0, 0, 0.0));
+			grad[y*height+x].vec = newVector;
+		}
+	}
+	printf("%f\n", grad[0].vec[0]);
+	printf("%f\n", grad[0].vec[1]);
+	return grad;
 }
