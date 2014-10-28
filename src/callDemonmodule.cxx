6c429ef10f48525ecb44233cd6d803f16be0d4e5
Thiago Nunes <gorobaum@gmail.com>
2014-10-28 16:50:00 -0200

    reading one voxel

--- src/callDemonmodule.cxx
+++ src/callDemonmodule.cxx
@@ -17,9 +17,10 @@ callDemon(PyObject *self, PyObject *args)
 
     array = (PyArrayObject*)PyArray_FROM_OTF(dataAux, 2, 0);
 
-    std::cout << "Tipo = " << PyArray_DTYPE(array) << "\n";
-    std::cout << "É continuo = " << PyArray_ISCONTIGUOUS(array) << "\n";
-    std::cout << "[0][0][0] = " << static_cast<int*>(PyArray_GETPTR3(array, 0, 0, 0)) << "\n";
+    std::cout << "Dimensoes = " << PyArray_NDIM(array) << "\n";
+    std::cout << "Tipo = " << PyArray_DESCR(array)->kind << "\n";
+    unsigned char * macaco = static_cast<unsigned char*>(PyArray_GETPTR3(array, 0, 0, 0));
+    std::cout << "[0][0][0] = " << (int)*macaco << "\n";
 
     Py_DECREF(array);
     Py_INCREF(Py_None);
